b01cdb3c74e6f14a05210cd620837338b24c0f2d
KseniaStu <studilina.ksenia@gmail.com>
2021-02-13 17:45:06 -0500

    project set up files

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+
+require_once 'vendor/autoload.php'; 
+
+//DB connection here
+
+
+// Create and configure Slim app
+$config = ['settings' => [
+    'addContentLengthHeader' => false,
+]];
+$app = new \Slim\App($config); 
+
+// Fetch DI Container
+$container = $app->getContainer();
+
+// Register Twig View helper
+$container['view'] = function ($c) {
+    $view = new \Slim\Views\Twig(dirname(__FILE__) . '/templates', [
+        'cache' => dirname(__FILE__) . '/tmplcache',
+        'debug' => true, // This line should enable debug mode
+    ]);
+    //
+    $view->getEnvironment()->addGlobal('test1','VALUE');
+    // Instantiate and add Slim specific extension
+    $router = $c->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+    return $view;
+};
+
+// Define app routes here
+
+
+// Run app
+$app->run(); 
