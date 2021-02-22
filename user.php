fe2151aa2023758e241bac0f450109bc8bc5f2ac
KseniaStu <studilina.ksenia@gmail.com>
2021-02-22 01:47:44 -0500

    updating client information

--- user.php
+++ user.php
@@ -41,14 +41,14 @@ $app->get('/isemailtaken/[{email}]', function ($request, $response, $args) {
 //User tries to log in
 $app->post('/login', function ($request, $response, $args) use ($log) {
     $email = $request->getParam('email');
-    $password = $request->getParam('password'); 
+    $password = $request->getParam('password');
 
     //get user from db by email
     $user = DB::queryFirstRow("SELECT * FROM users WHERE email=%s", $email);
     $loginSuccess = false;
     $errorList = array();
     if ($user && ($password == $user['password'])) {
-        $loginSuccess = true;  
+        $loginSuccess = true;
     } else {
         $errorList[] =  "Wrong email or password";
     }
@@ -59,9 +59,9 @@ $app->post('/login', function ($request, $response, $args) use ($log) {
         return $this->view->render($response, 'login.html.twig', ['errorList' => $errorList]);
     } else { //login successful
         unset($user['password']);
-        $_SESSION['user'] = $user; 
+        $_SESSION['user'] = $user;
         $log -> debug(sprintf("Login successful for email %s, uid=%d, from %s", $email, $user['id'], $_SERVER['REMOTE_ADDR']));
-        return $this->view->render($response, 'index.html.twig', ['userSession' => $_SESSION['user']]);    
+        return $this->view->render($response, 'index.html.twig', ['userSession' => $_SESSION['user']]);
     }
 });
 
@@ -72,7 +72,7 @@ $app->post('/login', function ($request, $response, $args) use ($log) {
 $app->get('/logout', function ($request, $response, $args) use ($log) {
     unset($_SESSION['user']);
     unset($_SESSION['access_token']);
-    return $this->view->render($response, 'index.html.twig', ['userSession' => null]);    
+    return $this->view->render($response, 'index.html.twig', ['userSession' => null]);
 });
 
 //registration page
@@ -106,18 +106,18 @@ $app->post('/register', function ($request, $response, $args) {
     if ((strlen($address) < 5) || (strlen($address) > 200)) {
             $errorList[] = "Address must be between 5 and 200 characters";
             $address="";
-    } 
+    }
 
     //verify postal code
     if (preg_match('/^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/', $postalCode) != 1) {
-        $errorList[] = "Postal code must be in the A1A1A1 or A1A 1A1 format"; 
-        $postalCode = "";   
+        $errorList[] = "Postal code must be in the A1A1A1 or A1A 1A1 format";
+        $postalCode = "";
     }
 
     //verify phone number
     if (preg_match('/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/', $phone) != 1) {
         $errorList[] = "Phone must be in 000-000-00-00 format";
-        $phone = "";    
+        $phone = "";
     }
 
     //verify date of birth
@@ -128,19 +128,19 @@ $app->post('/register', function ($request, $response, $args) {
 
     //verify first name
     if ((strlen($firstName) < 1) || (strlen($firstName) > 20)) {
-        $errorList[] = "First name must be between 1 and 20 characters";    
+        $errorList[] = "First name must be between 1 and 20 characters";
         $firstName = "";
     }
 
     //verify last name
     if ((strlen($lastName) < 1) || (strlen($lastName) > 20)) {
-        $errorList[] = "Last name must be between 1 and 20 characters";  
-        $lastName = "";  
+        $errorList[] = "Last name must be between 1 and 20 characters";
+        $lastName = "";
     }
 
     //verify description
     if ((strlen($description) < 10) || (strlen($description) > 500)) {
-        $errorList[] = "Description must be between 10 and 500 characters";    
+        $errorList[] = "Description must be between 10 and 500 characters";
         $description = "";
     }
 
@@ -165,8 +165,8 @@ $app->post('/register', function ($request, $response, $args) {
             || (preg_match("/[A-Z]/", $password) == FALSE )
             || (preg_match("/[a-z]/", $password) == FALSE )
             || (preg_match("/[0-9]/", $password) == FALSE )) {
-            $errorList[] = "Password must be between 6 and 20 characters and contain at least one uppercase, one lowercase, and one digit";    
-        }                                                  
+            $errorList[] = "Password must be between 6 and 20 characters and contain at least one uppercase, one lowercase, and one digit";
+        }
     }
 
     // verify photo
@@ -178,30 +178,110 @@ $app->post('/register', function ($request, $response, $args) {
         $result = verifyUploadedPhoto($photo, $mimeType);
         if ($result !== TRUE) {
             $errorList[] = $result;
-        } 
+        }
     }
     else {
         $errorList[] = "Photo must be uploaded";
     }
 
 
-    if ($errorList) {                  
-        return $this->view->render($response, 'register.html.twig', ['errorList' => $errorList, 
+    if ($errorList) {
+        return $this->view->render($response, 'register.html.twig', ['errorList' => $errorList,
         'value'=> [ 'role' => $role, 'address' => $address, 'postalCode' => $postalCode, 'phone' => $phone,
         'gender' => $gender, 'dateOfBirth' => $dateOfBirth, 'firstName' => $firstName, 'lastName' => $lastName, 'description' => $description,
         'email' => $email] ]);
     }
     else {
         $photoData = file_get_contents($photo->file);
-        DB::insert('users', [ 'firstName' => $firstName, 'lastName' => $lastName, 'gender' => $gender, 
+        DB::insert('users', [ 'firstName' => $firstName, 'lastName' => $lastName, 'gender' => $gender,
         'dateOfBirth' => $dateOfBirth, 'phoneNo' => $phone, 'address' => $address, 'postalCode' => $postalCode,
-        'email' => $email, 'password' => $password, 'role' => $role, 'photo' => $photoData, 'description' => $description]);    
-        return $this->view->render($response, 'login.html.twig');            
-    } 
+        'email' => $email, 'password' => $password, 'role' => $role, 'photo' => $photoData, 'description' => $description]);
+        return $this->view->render($response, 'login.html.twig');
+    }
 });
 
 
+// user updates account
+$app->post('/accountcaregiver', function ($request, $response, $args) {
+    $services = $request->getParam('serv');
+    $address = $request->getParam('address');
+    $postalCode = $request->getParam('postal');
+    $phone = $request->getParam('phone');
+    $firstName = $request->getParam('firstName');
+    $lastName = $request->getParam('lastName');
+    $description = $request->getParam('description');
+    $photo = $request->getUploadedFiles()['photo'];
 
+    $errorList = array();
+
+    //verify address
+    if ((strlen($address) < 5) || (strlen($address) > 200)) {
+            $errorList[] = "Address must be between 5 and 200 characters";
+            $address="";
+    }
+
+    //verify postal code
+    if (preg_match('/^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/', $postalCode) != 1) {
+        $errorList[] = "Postal code must be in the A1A1A1 or A1A 1A1 format";
+        $postalCode = "";
+    }
+
+    //verify phone number
+    if (preg_match('/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/', $phone) != 1) {
+        $errorList[] = "Phone must be in 000-000-00-00 format";
+        $phone = "";
+    }
+
+    //verify first name
+    if ((strlen($firstName) < 1) || (strlen($firstName) > 20)) {
+        $errorList[] = "First name must be between 1 and 20 characters";
+        $firstName = "";
+    }
+
+    //verify last name
+    if ((strlen($lastName) < 1) || (strlen($lastName) > 20)) {
+        $errorList[] = "Last name must be between 1 and 20 characters";
+        $lastName = "";
+    }
+
+    //verify description
+    if ((strlen($description) < 10) || (strlen($description) > 500)) {
+        $errorList[] = "Description must be between 10 and 500 characters";
+        $description = "";
+    }
+
+/*    // verify photo
+    $hasPhoto = false;
+    $mimeType = "";
+    if ($photo->getError() != UPLOAD_ERR_NO_FILE) { // was image uploaded?
+        // print_r($uploadedImage->getError());
+        $hasPhoto = true;
+        $result = verifyUploadedPhoto($photo, $mimeType);
+        if ($result !== TRUE) {
+            $errorList[] = $result;
+        }
+    }
+    else {
+        $errorList[] = "Photo must be uploaded";
+    } */
+
+    if ($errorList) {
+        return $this->view->render($response, 'accountcaregiver.html.twig', ['errorList' => $errorList,
+        'value'=> [ 'address' => $address, 'postalCode' => $postalCode, 'phone' => $phone,
+         'firstName' => $firstName, 'lastName' => $lastName, 'description' => $description] ]);
+    }
+    else {
+        $photoData = file_get_contents($photo->file);
+        DB::query("UPDATE users SET address=%s, postalCode=%s, phoneNo=%s, firstName=%s,
+        lastName=%s, description=%s  WHERE id=%d", $address, $postalCode, $phone, $firstName,
+        $lastName, $description, $_SESSION['user']['id']);
+
+        $user = DB::queryFirstRow("SELECT * FROM users WHERE id = %d LIMIT 1", $_SESSION['user']['id']);
+        $_SESSION['user'] = $user;
+        return $this->view->render($response, 'accountcaregiver.html.twig', ['success' => "Account was successfully updated",
+        'userSession' => $user]);
+    }
+});
 
 //function to verify photo
 // returns TRUE on success
@@ -209,7 +289,7 @@ $app->post('/register', function ($request, $response, $args) {
 function verifyUploadedPhoto($photo, &$mime = null) {
     if ($photo->getError() != 0) {
         return "Error uploading photo " . $photo->getError();
-    } 
+    }
     if ($photo->getSize() > 1024*1024) { // 1MB
         return "File too big. 1MB max is allowed.";
     }
@@ -229,14 +309,14 @@ function verifyUploadedPhoto($photo, &$mime = null) {
         case 'image/png': $ext = "png"; break;
         default:
             return "Only JPG, GIF and PNG file types are allowed";
-    } 
+    }
     if (!is_null($mime)) {
         $mime = $info['mime'];
     }
     return TRUE;
 }
-  
- 
+
+
 
 //is email uqnique
 
